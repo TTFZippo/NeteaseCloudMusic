681e8d8060c1f933f6bf7712f1cfb6f0e4ff94ba
zzbs-yyx <2232759109@qq.com>
2021-04-11 22:43:20 +0800

    新碟上架轮播图

--- cloudmusic/src/components/album.vue
+++ cloudmusic/src/components/album.vue
@@ -1,16 +1,35 @@
 <!-- 专辑（碟） -->
 <template>
-  <div class="wrapper">
-    {{albumData.name}}
-    {{albumData.picUrl}}
+  <div class="album-wrapper">
+    <div class="cover">
+      <img :src="albumData.picUrl" alt="">
+      <a href="javascript:;" class="mask"></a>
+      <a href="javascript:;" class="icon-play"></a>
+    </div>
+    <a href="javascript:;" :title="albumData.name">{{albumData.name}}</a>
+    <a href="javascript:;" :title="artists" class="artists">{{artists}}</a>
   </div>
 </template>
 
 <script>
 export default {
+  created () {
+    // console.log(this.albumData);
+  },
   props: {
     albumData: {
       type: Object
+    }
+  },
+  computed: {
+    // 创作者
+    artists() {
+      let allArtists = "";
+      this.albumData.artists.forEach((item, index) => {
+        // console.log(index);
+        allArtists += index==0 ? `${item.name}` : ` \\ ${item.name}`
+      })
+      return allArtists
     }
   },
   data () {
@@ -22,9 +41,56 @@ export default {
 </script>
 
 <style scoped>
-.wrapper {
-  width: 200px;
-  height: 200px;
-  border: 1px solid red;
+* {
+  margin: 0;
+  padding: 0;
+}
+.album-wrapper {
+  margin: 10px 8px 0 8px;
+  width: 118px;
+  height: 150px;
+}
+.album-wrapper .cover {
+  position: relative;
+}
+.album-wrapper .cover img {
+  width: 100px;
+  height: 100px;
+}
+.album-wrapper .cover .mask {
+  display: block;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 118px;
+  height: 100px;
+  background-image: url('../assets/icon/coverall.png');
+  background-position: 0 -570px;
+}
+.album-wrapper .cover .icon-play:hover,
+.album-wrapper .cover .mask:hover+.icon-play {
+  display: block;
+}
+.album-wrapper .cover .icon-play {
+  display: none;
+  position: absolute;
+  left: 72px;
+  bottom: 8px;
+  width: 22px;
+  height: 22px;
+  background-image: url('../assets/icon/iconall.png');
+  background-position: 0 -85px;
+}
+.album-wrapper a{
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  display: block;
+  font-size: 12px;
+  text-decoration: none;
+  color: black;
+}
+.album-wrapper .artists {
+  color: #666;
 }
 </style>
--- cloudmusic/src/views/discover/discover_subpage/recommand/recommand_subpage/newAlbum.vue
+++ cloudmusic/src/views/discover/discover_subpage/recommand/recommand_subpage/newAlbum.vue
@@ -10,27 +10,106 @@
     </div>
     <!-- 轮播图 -->
     <div class="swiper">
-      <album v-for="(item, index) in albums"  :key="index" :albumData="item"></album>
+      <a href="javascript:;" class="previous" @click="previous"></a>
+      <main class="swiper-content">
+        <div
+          class="roll"
+          :style="{
+            transform: `translate(-${101 * activeIndex}%)`,
+            transition: istransition ? 'transform 1s linear' : ''
+          }"
+        >
+          <div
+            class="swiper-part"
+            v-for="(item, index) in allAlbums"
+            :key="index"
+          >
+            <album
+              v-for="(albumItem, albumIndex) in item"
+              :key="albumIndex"
+              :albumData="albumItem"
+            ></album>
+          </div>
+        </div>
+      </main>
+      <a href="javascript:;" class="next" @click="next"></a>
     </div>
   </div>
 </template>
 
 <script>
-import album from '../../../../../components/album.vue'
+import album from "../../../../../components/album.vue";
 export default {
-  created () {
-    
+  created() {
+    // console.log(this.ten_albums);
   },
   components: {
-    album
+    album,
+  },
+  computed: {
+    albums_part1() {
+      return this.albums.filter(
+        (item, index, array) => index > 4 && index <= 9
+      );
+    },
+    albums_part2() {
+      return this.albums.filter(
+        (item, index, array) => index >= 0 && index <= 4
+      );
+    },
+    allAlbums() {
+      let allAlbums = [
+        this.albums_part2,
+        this.albums_part1,
+        this.albums_part2,
+        this.albums_part1,
+      ];
+      return allAlbums;
+    },
   },
   props: {
     albums: {
-      type: Array
-    }
+      type: Array,
+    },
   },
   data() {
-    return {};
+    return {
+      activeIndex: 0,
+      istransition: true // 临界切换activeIndex时关闭
+    };
+  },
+  methods: {
+    // 上一张
+    previous() {
+      // 到达左边临界
+      if(this.activeIndex == 0)  {
+        this.istransition = false;
+        this.activeIndex = 2;
+        setTimeout(() => {
+          this.istransition = true;
+          this.activeIndex--;
+        }, 10);
+      } else {
+        this.istransition = true;
+        this.activeIndex--;
+      }
+      console.log(this.activeIndex);
+    },
+    // 下一张
+    next() {
+      if(this.activeIndex == 3)  {
+        this.istransition = false;
+        this.activeIndex = 1;
+        setTimeout(() => {
+          this.istransition = true;
+          this.activeIndex++;
+        }, 10);
+      } else {
+        this.istransition = true;
+        this.activeIndex++;
+      }
+      console.log(this.activeIndex);
+    },
   },
 };
 </script>
@@ -41,7 +120,7 @@ export default {
   margin-bottom: 50px;
 }
 /* 热门推荐顶部信息 */
- .label {
+.label {
   display: flex;
   padding: 0 10px 0 34px;
   margin-bottom: 10px;
@@ -80,11 +159,11 @@ export default {
   text-decoration: none;
   color: #333;
 }
-.label .more i{
+.label .more i {
   display: inline-block;
   width: 12px;
   height: 12px;
-  background: url('../../../../../assets/icon/index.png') no-repeat 0 9999px;
+  background: url("../../../../../assets/icon/index.png") no-repeat 0 9999px;
   background-position: 0 -240px;
 }
 .label .more a:hover,
@@ -92,5 +171,47 @@ export default {
   text-decoration: underline;
   cursor: pointer;
 }
+.swiper {
+  position: relative;
+  display: flex;
+  background-color: rgb(245, 245, 245);
+}
+.swiper .swiper-content {
+  margin-top: 20px;
+  height: 180px;
+  position: relative;
+  display: flex;
+  overflow: hidden;
+  width: 95%;
+}
+.swiper .swiper-content .roll {
+  display: flex;
+  position: relative;
+  width: 100%;
+}
+.swiper .swiper-content .roll .swiper-part {
+  display: flex;
+  transition: left 1s linear;
+}
 
+.swiper .previous,
+.swiper .next {
+  width: 17px;
+  height: 17px;
+  flex-basis: 17px;
+  align-self: center;
+  background: url("../../../../../assets/icon/index.png");
+}
+.swiper .previous {
+  background-position: -260px -75px;
+}
+.swiper .previous:hover {
+  background-position: -280px -75px;
+}
+.swiper .next {
+  background-position: -300px -75px;
+}
+.swiper .next:hover {
+  background-position: -320px -75px;
+}
 </style>
